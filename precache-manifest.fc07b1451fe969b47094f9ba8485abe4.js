a6a85453ff680677bb0baba600a694e5b71fbc93
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-06-04 05:46:07 +0100

    Updates

---
+++ precache-manifest.fc07b1451fe969b47094f9ba8485abe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cbad66a9436970bd48cc36493dbea7",
+    "url": "/Monster-s-Rolodex/index.html"
+  },
+  {
+    "revision": "3e7fa8ff3b430f4f40d1",
+    "url": "/Monster-s-Rolodex/static/css/main.07fe4f90.chunk.css"
+  },
+  {
+    "revision": "444fdb9fa08a8d5b0d5b",
+    "url": "/Monster-s-Rolodex/static/js/2.1572ac59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-s-Rolodex/static/js/2.1572ac59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7fa8ff3b430f4f40d1",
+    "url": "/Monster-s-Rolodex/static/js/main.dde99142.chunk.js"
+  },
+  {
+    "revision": "7ef7f4c573e0ffd54d58",
+    "url": "/Monster-s-Rolodex/static/js/runtime-main.66cbe20a.js"
+  }
+]);
